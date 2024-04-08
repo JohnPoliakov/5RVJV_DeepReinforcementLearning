e4e42665889ca670c55208af83ebe3c9fc1e3ba3
LouisF <54190296+LouisFoschiani@users.noreply.github.com>
2024-04-08 14:15:48 +0200

    Good grid + camera

--- DRL/Assets/Scripts/GridManager.cs
+++ DRL/Assets/Scripts/GridManager.cs
@@ -6,61 +6,53 @@ public class GridManager : MonoBehaviour
 {
     public int width = 5;
     public int height = 5;
-    public GameObject cellPrefab; // Prefab pour une cellule basique
-    public GameObject startPrefab; // Prefab pour le point de départ
-    public GameObject endPrefab; // Prefab pour le point d'arrivée
-    public GameObject obstaclePrefab; // Prefab pour les obstacles
+    public GameObject cellPrefab;
+    public GameObject startPrefab;
+    public GameObject endPrefab;
+    public GameObject obstaclePrefab;
 
-    private List<Vector3> usedPositions = new List<Vector3>(); // Liste pour stocker les positions déjà utilisées
-    private Vector3 startPosition;
-    private Vector3 endPosition;
+    private List<Vector3> usedPositions = new List<Vector3>();
 
     void Start()
     {
         GenerateGrid();
     }
 
-    void GenerateGrid()
-    {
-        startPosition = PlaceSpecialCell(startPrefab);
-        endPosition = PlaceEndCell(endPrefab);
-        PlaceSpecialCell(obstaclePrefab); 
-        PlaceSpecialCell(obstaclePrefab);
-        PlaceSpecialCell(obstaclePrefab);
-        PlaceSpecialCell(obstaclePrefab);
+    void GenerateGrid(){
+    Vector3 startPosition = new Vector3(0, 0, 0);
+    Instantiate(startPrefab, startPosition, Quaternion.identity);
+    usedPositions.Add(startPosition);
 
-        for (int x = 0; x < width * cellPrefab.transform.localScale.x; x += (int)cellPrefab.transform.localScale.x)
+    Vector3 endPosition = new Vector3((width - 1) * cellPrefab.transform.localScale.x, 0, (height - 1) * cellPrefab.transform.localScale.z);
+    Instantiate(endPrefab, endPosition, Quaternion.identity);
+    usedPositions.Add(endPosition);
+
+    PlaceSpecialCell(obstaclePrefab);
+    PlaceSpecialCell(obstaclePrefab);
+    PlaceSpecialCell(obstaclePrefab);
+    PlaceSpecialCell(obstaclePrefab);
+    PlaceSpecialCell(obstaclePrefab);
+    PlaceSpecialCell(obstaclePrefab);
+    PlaceSpecialCell(obstaclePrefab);
+
+    for (int x = 0; x < width * cellPrefab.transform.localScale.x; x += (int)cellPrefab.transform.localScale.x)
+    {
+        for (int z = 0; z < height * cellPrefab.transform.localScale.z; z += (int)cellPrefab.transform.localScale.z)
         {
-            for (int z = 0; z < height * cellPrefab.transform.localScale.z; z += (int)cellPrefab.transform.localScale.z)
+            Vector3 position = new Vector3(x, 0, z);
+            if (!usedPositions.Contains(position))
             {
-                Vector3 position = new Vector3(x, 0, z);
-                if (!usedPositions.Contains(position))
-                {
-                    Instantiate(cellPrefab, position, Quaternion.identity);
-                }
+                Instantiate(cellPrefab, position, Quaternion.identity);
             }
         }
     }
+}
 
-    Vector3 PlaceSpecialCell(GameObject prefab)
+    void PlaceSpecialCell(GameObject prefab)
     {
         Vector3 position = GetRandomPosition();
         Instantiate(prefab, position, Quaternion.identity);
         usedPositions.Add(position);
-        return position;
-    }
-
-    Vector3 PlaceEndCell(GameObject prefab)
-    {
-        Vector3 position;
-        do
-        {
-            position = GetRandomPosition();
-        }
-        while (ManhattanDistance(startPosition, position) < 5);
-        Instantiate(prefab, position, Quaternion.identity);
-        usedPositions.Add(position);
-        return position;
     }
 
     Vector3 GetRandomPosition()
@@ -68,6 +60,7 @@ public class GridManager : MonoBehaviour
         int x = Random.Range(0, width) * (int)cellPrefab.transform.localScale.x;
         int z = Random.Range(0, height) * (int)cellPrefab.transform.localScale.z;
         Vector3 position = new Vector3(x, 0, z);
+        // S'assure que la position n'a pas déjà été choisie
         while (usedPositions.Contains(position))
         {
             x = Random.Range(0, width) * (int)cellPrefab.transform.localScale.x;
@@ -75,10 +68,5 @@ public class GridManager : MonoBehaviour
             position = new Vector3(x, 0, z);
         }
         return position;
-    }
-
-    int ManhattanDistance(Vector3 a, Vector3 b)
-    {
-        return Mathf.Abs((int)(a.x - b.x)) + Mathf.Abs((int)(a.z - b.z));
     }
 }
